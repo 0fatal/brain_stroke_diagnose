fb6e884c8b53211dcef80576c99d64d08020c896
ZacharyWin <2816813070@qq.com>
2022-05-01 23:52:28 +0800

    chore

--- packages/renderer/src/main.tsx
+++ packages/renderer/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
@@ -7,7 +8,9 @@ const root = document.getElementById('root');
 
 render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   root
 );
---
+++ packages/renderer/src/router/index.tsx
@@ -0,0 +1,9 @@
+import { RouteObject } from 'react-router-dom';
+import Layout from '@/layout';
+
+const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <Layout />
+  }
+];
