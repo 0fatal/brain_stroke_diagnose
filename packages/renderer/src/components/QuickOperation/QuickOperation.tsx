0d3c78958bd717d493c5555b5ad993b60025de3d
ZacharyWin <2816813070@qq.com>
2022-05-02 10:35:08 +0800

    feat(components): add `QuickOperation`

---
+++ packages/renderer/src/components/QuickOperation/QuickOperation.tsx
@@ -0,0 +1,15 @@
+import { Button } from 'antd';
+import { useNavigate } from 'react-router-dom';
+
+const QuickOperation = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex space-x-4">
+      <Button onClick={() => navigate('/diagnose')}>病情诊断</Button>
+      <Button onClick={() => navigate('/record')}>诊断记录</Button>
+    </div>
+  );
+};
+
+export default QuickOperation;
