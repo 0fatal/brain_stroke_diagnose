c6e3b3b89270f8c9637053ff7ab25d6d3e97dd30
ZacharyWin <2816813070@qq.com>
2022-05-02 10:36:48 +0800

    chore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './packages/renderer/index.html',
+    './packages/renderer/src/**/*.{vue,js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
