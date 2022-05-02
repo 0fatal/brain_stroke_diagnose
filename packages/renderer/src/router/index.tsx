95095f3534fa50f81c95688f95c2034d1f935eaa
ZacharyWin <2816813070@qq.com>
2022-05-02 10:36:34 +0800

    feat: add router

--- packages/renderer/src/router/index.tsx
+++ packages/renderer/src/router/index.tsx
@@ -1,9 +1,68 @@
-import { RouteObject } from 'react-router-dom';
-import Layout from '@/layout';
+import { RouteObject, useRoutes } from 'react-router-dom';
+import ZLayout from '@/layout';
+import DiagnosePage from '@/pages/Diagnose';
+import RecordPage from '@/pages/Record';
+import DevicePage from '@/pages/Device';
+import AnalysisPage from '@/pages/Analysis';
+import UserManagementPage from '@/pages/Management/User';
+import PermissionManagementPage from '@/pages/Management/Permission';
+import LogManagementPage from '@/pages/Management/Log';
+
+const routeTitleMap = {
+  '/diagnose': '病情诊断',
+  '/record': '诊断记录',
+  '/device': '设备管理',
+  '/analysis': '数据概况',
+  '/management/user': '系统管理 / 用户管理',
+  '/management/permission': '系统管理 / 权限管理',
+  '/management/log': '系统管理 / 日志管理'
+} as const;
+
+export const getRouteTitle = (route: string) => {
+  // @ts-ignore
+  return routeTitleMap[route];
+};
 
 const routes: RouteObject[] = [
   {
     path: '/',
-    element: <Layout />
+    element: <ZLayout />,
+    children: [
+      { path: '/diagnose', element: <DiagnosePage /> },
+      {
+        path: '/record',
+        element: <RecordPage />
+      },
+      {
+        path: '/management',
+        children: [
+          {
+            path: '/management/user',
+            element: <UserManagementPage />,
+            index: true
+          },
+          {
+            path: '/management/permission',
+            element: <PermissionManagementPage />
+          },
+          {
+            path: '/management/log',
+            element: <LogManagementPage />
+          }
+        ]
+      },
+      {
+        path: '/device',
+        element: <DevicePage />
+      },
+      {
+        path: '/analysis',
+        element: <AnalysisPage />
+      }
+    ]
   }
 ];
+
+export default function useAppRoutes() {
+  return useRoutes(routes);
+}
